a699cf79bee0e39b61a505db67a0f1b737d62ac1
qdqd <qdqdqdqdqd@protonmail.com>
2022-06-23 16:19:28 +0200

    chore: improve security by checking tree depth before verify
    That way, we're preventing against second preimage attack.

--- scripts/generateMerkle.ts
+++ scripts/generateMerkle.ts
@@ -17,9 +17,13 @@ function geneateMerkle() {
   // get the root hash of the merkleTree
   const rootHash = merkleTree.getRoot().toString("hex");
 
+  // number of valid proof require to valid the merkleTree
+  const proofLength = merkleTree.getProof(leafsNodes[0]).length;
+
   return {
     rootHash,
     leafsNodes,
+    proofLength,
     // A Merkle Tree with n leaves has O(log2 n) sized proof
     generateProof: (address: string) =>
       merkleTree.getHexProof(ethers.utils.keccak256(address)),
